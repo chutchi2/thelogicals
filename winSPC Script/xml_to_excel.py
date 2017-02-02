9c717fd3e687e86643e66348758c153ef742746c
Cody Hutchins <chutchi2@uccs.edu>
2017-02-01 20:48:40 -0700

    Updated script to working revision

--- winSPC Script/xml_to_excel.py
+++ winSPC Script/xml_to_excel.py
@@ -6,15 +6,10 @@ from datetime import datetime
 tree = ET.parse('Saturn[1234].xml')
 root = tree.getroot()
 
-wb = xlsxwriter.Workbook(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")+'conversions.xlsx')
+wb = xlsxwriter.Workbook(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")+'_xml_conversion.xlsx')
 ws = wb.add_worksheet()
 	
 value = root.findall('.//Value')
-#for index,child in enumerate(root.findall('.//Value'),start=0):
-#	ws.write(index,0,child.text)
-#	print (child.attrib)
-#	if child.get('ID') is not None:
-#		print (child.get('ID'))
 count = 0
 for Value in root.findall('.//Value'):
 	if Value.get('ID') is not None:
@@ -23,25 +18,16 @@ for Value in root.findall('.//Value'):
 				for Value in Prop:
 					if Value is not None:
 						count = count + 1
-						ws.write(1,count,Value.text)
-						#print(Value.text)
+						ws.write(count,1,Value.text)
 			elif Prop.get('Name') == 'Numeric':
 				for Value in Prop:
 					if Value is not None:
-						#count = count + 1
-						ws.write(2,count,Value.text)
-						#print(Value.text)
+						ws.write(count,2,Value.text)
 			elif Prop.get('Name') == 'TS':
-				#print(Prop.get('Name'))
 				for Prop in Prop:
 					if Prop is not None:
 						if Prop.get('Name') == 'StepName':
 							for Value in Prop:
 								if Value is not None:
-									#count = count + 1
-									ws.write(0,count,Value.text)
-									print(Value.text)
-							
-
-
+									ws.write(count,0,Value.text)
 wb.close()
